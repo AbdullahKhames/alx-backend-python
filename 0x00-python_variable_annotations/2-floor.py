994da4ec44fc1ecb0da489632981c19338d5827e
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-11 21:54:20 +0200

    type checking with annotations

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,4 +4,9 @@ import math
 
 
 def floor(n: float) -> int:
+    """
+    returns a floor of the float
+    :param n: the float to floor
+    :return: the int floor value
+    """
     return math.floor(n)
