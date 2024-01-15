ddf45a44b6be06e57cd2ce86346534e7f602be90
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-16 00:20:39 +0200

    await asyncio

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """module to test basic async
 """
+import asyncio
 
 
-def foo():
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """function to test async operations
     """
+    return asyncio.create_task()
