a1b8d72b7268f64b8ddabc6083056052507f8881
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-15 23:50:54 +0200

    await asyncio

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """function to test async operations
     max_delay: maximum delay
     """
