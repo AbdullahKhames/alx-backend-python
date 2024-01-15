778c94e65066d67e284d33fe855cbe36e7742088
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-16 00:44:19 +0200

    await asyncio

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """module to test basic async
 """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def foo():
+async def task_wait_n(n: int, max_delay: int):
     """function to test async operations
     """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    
+    delays = await asyncio.gather(*tasks)
+
+    return delays
