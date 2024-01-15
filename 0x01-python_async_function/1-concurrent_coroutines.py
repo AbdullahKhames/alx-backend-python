c2820bff1880f2dda0820f8622789ec77a7c9b60
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-16 00:17:59 +0200

    await asyncio

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """module to test basic async
 """
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-def foo():
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """function to test async operations
     """
+    delays: List[float] = []
+    futures = []
+    for _ in range(n):
+        futures.append(wait_random(max_delay))
+    for future in asyncio.as_completed(futures):
+        delay = await future
+        delays.append(delay)
+    return delays
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """module to test basic async
 """
-
-
-def foo():
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+def measure_time(n: int, max_delay: int) -> float:
     """function to test async operations
     """
+    before = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    after = time.time()
+    total_time = after - before
+    return total_time / n
