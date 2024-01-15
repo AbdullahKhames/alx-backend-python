7250a9d1014ae63a9d3f69f16287b77dfd5863d0
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-16 00:21:57 +0200

    await asyncio

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,9 +2,10 @@
 """module to test basic async
 """
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """function to test async operations
     """
-    return asyncio.create_task()
+    return asyncio.create_task(wait_random(max_delay))
