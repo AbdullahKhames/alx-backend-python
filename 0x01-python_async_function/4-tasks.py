75fbd96cf6257ed0f7bb89eb4d9e2f9cfd4b7a9e
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-17 01:28:11 +0200

    await asyncio

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,8 +9,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """function to test async operations
     """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-
-    delays = await asyncio.gather(*tasks)
-
+    delays: List[float] = []
+    futures = []
+    for _ in range(n):
+        futures.append(wait_random(max_delay))
+    for future in asyncio.as_completed(futures):
+        delay = await future
+        delays.append(delay)
     return delays
